e7a8d4173e3a3755ed1efa709deb6889c0a8b82e
Vaishak Suresh <vaishak.suresh@gmail.com>
2013-11-25 23:18:51 -0800

    Write directly to MongoDB

--- mongo/read_events.py
+++ mongo/read_events.py
@@ -1,11 +1,10 @@
 __author__ = 'vaishaksuresh'
-import json
+try:
+    import simplejson as json
+except ImportError:
+    import json
 import os
-
-push_data = []
-watch_data = []
-follow_data = []
-
+from pymongo import MongoClient
 
 def is_push_event(event_data):
 
@@ -50,18 +49,28 @@ def process_files(file_name):
     watch_events = filter(is_watch_event, data)
     follow_events = filter(is_follow_event, data)
 
+    client = MongoClient()
+    db = client.github_events
+    push_collection = db.push_events
+    watch_collection = db.watch_events
+    follow_collection = db.follow_events
+
+
     for line in push_events:
-        push_data.append(json.loads(unicode(line, errors='replace')))
+        #push_data.append(json.loads(unicode(line, errors='replace')))
+        push_collection.save(json.loads(unicode(line, errors='replace')))
 
     for line in watch_events:
-        watch_data.append(json.loads(unicode(line, errors='replace')))
+        #watch_data.append(json.loads(unicode(line, errors='replace')))
+        watch_collection.save(json.loads(unicode(line, errors='replace')))
 
     for line in follow_events:
-        follow_data.append(json.loads(unicode(line, errors='replace')))
+        #follow_data.append(json.loads(unicode(line, errors='replace')))
+        follow_collection.save(json.loads(unicode(line, errors='replace')))
 
 
 def main():
-    for dirname, dirnames, filenames in os.walk('./data'):
+    for dirname, dirnames, filenames in os.walk('../data'):
         dirnames.remove('processed')
         if '.git' in dirnames:
             dirnames.remove('.git')
@@ -70,12 +79,6 @@ def main():
                 print os.path.join(dirname, filename)
                 process_files(os.path.join(dirname, filename))
 
-    with open('./data/processed/push_events.json', 'a') as outfile:
-        json.dump(push_data, outfile, indent=2)
-    with open('./data/processed/watch_events.json', 'a') as outfile:
-        json.dump(watch_data, outfile, indent=2)
-    with open('./data/processed/follow_events.json', 'a') as outfile:
-        json.dump(follow_data, outfile, indent=2)
 
 if __name__ == '__main__':
     main()
