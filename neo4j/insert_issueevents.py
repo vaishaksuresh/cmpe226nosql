119f3510695826ab00626c29eb5177f411dd369e
abbasiec <eccaldav@gmail.com>
2013-11-30 14:02:33 -0800

    Read multiple files.

--- neo4j/insert_issueevents.py
+++ neo4j/insert_issueevents.py
@@ -1,8 +1,6 @@
 import json
 from py2neo import neo4j, node, rel
-
-f = open("file1.json", "r")
-data = json.loads(f.read())
+import os
 
 def is_issueevent_and_opened(event):
     return event[u'type'] == u'IssuesEvent' and event[u'payload'][u'action'] == u'opened' 
@@ -20,38 +18,78 @@ def top_level_properties(event):
     return newEvent
             
     
-
-issueevents = filter(is_issueevent, data)
-
 db = neo4j.GraphDatabaseService("http://localhost:7474/db/data")
 
-db.clear()
+# db.clear()
 
-for ie in issueevents:
-#     print ie
+def process_single_file_data(jsonArray):
+    data = json.loads(jsonArray)
+    issueevents = filter(is_issueevent, data)
+    for ie in issueevents:
+    #     print ie
+        
+        eventType = ie["type"]
+        repoId = ie["repo"]["id"]
+        repoName = ie["repo"]["name"]
+        
+        issueId = ie["payload"]["issue"]["id"]
+        issueTitle = ie["payload"]["issue"]["title"]
+        issueAction = ie["payload"]["action"]
+        issueCreatedAt = ie["payload"]["issue"]["created_at"]
+        
+#         print eventType
+#         print "repository:", repoId, repoName  
+#         print "issue:", issueId, issueAction, issueCreatedAt
+        
+        #remove null value and non-primitive values as properties cannot contain nested objects
+        issueProperties = top_level_properties(ie['payload']['issue'])
+#         print issueProperties
+#         print issueProperties["id"], issueProperties['action']
+        
+        repoNode = db.get_or_create_indexed_node(index_name="issueevents", key=repoId, value=repoName, properties=top_level_properties(ie['repo']))
+        repoNode.add_labels("REPOSITORY")
+        issueNode = db.get_or_create_indexed_node(index_name="issueevents", key=issueId, value=issueTitle, properties=issueProperties)
+        issueNode.add_labels("ISSUE")
+        db.create(rel(repoNode, ("ISSUE_" + issueAction, {"issue_created_at": issueCreatedAt, "issue_action":issueAction}), issueNode))
+        print "created relationship: "  + "ISSUE_" + str(issueAction) + "\tbetween reposirotyNode: " + str(repoId) + "\t and IssueNode: " + str(issueId)
+
+
+jsonFiles = []
+src = "./files"
+names = os.listdir(src)
+for file in names:
+    if file.endswith(".json"):
+        #print os.path.join(root, file)
+        jsonFiles.append(os.path.join(src, file));
+
     
-    eventType = ie["type"]
-    repoId = ie["repo"]["id"]
-    repoName = ie["repo"]["name"]
+# f = open("file1.json", "r")
+# data = json.loads(f.read())
+
+def jsonify_file(file):
+    f = open(file, "r")
+    jsonArray = ""
+    for line in f:
+        line = line.rstrip() + ','
+        jsonArray += line
     
-    issueId = ie["payload"]["issue"]["id"]
-    issueTitle = ie["payload"]["issue"]["title"]
-    issueAction = ie["payload"]["action"]
-    issueCreatedAt = ie["payload"]["issue"]["created_at"]
+    if(len(jsonArray.strip()) > 0):
+        jsonArray = jsonArray[:-1]
+        jsonArray2 = ''.join(('[', jsonArray, ']'))
     
-#     print eventType
-#     print "repository:", repoId, repoName  
-#     print "issue:", issueId, issueAction, issueCreatedAt
+        return jsonArray2
     
-    #remove null value and non-primitive values as properties cannot contain nested objects
-    issueProperties = top_level_properties(ie['payload']['issue'])
-#     print issueProperties
+    #return empty array
+    return []
     
-#     print issueProperties["id"], issueProperties['action']
+
+for file in jsonFiles:
+    print "processing file: " + file;
+    jsonArray = jsonify_file(file)
+    process_single_file_data(jsonArray)
     
-    repoNode = db.get_or_create_indexed_node(index_name="issueevents", key=repoId, value=repoName, properties=top_level_properties(ie['repo']))
-    repoNode.add_labels("REPOSITORY")
-    issueNode = db.get_or_create_indexed_node(index_name="issueevents", key=issueId, value=issueTitle, properties=issueProperties)
-    issueNode.add_labels("ISSUE")
-    db.create(rel(repoNode, ("ISSUE_" + issueAction, {"issue_created_at": issueCreatedAt, "issue_action":issueAction}), issueNode))
-    print "created relationship: "  + "ISSUE_" + str(issueAction) + "\tbetween reposirotyNode: " + str(repoId) + "\t and IssueNode: " + str(issueId)
+        
+        
+    
+    
+    
